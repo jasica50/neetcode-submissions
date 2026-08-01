2497e9bce562872c897da008970fa85ccefcf6a2
Jasica Aggarwal <jasicaaggarwal50@gmail.com>
2026-08-01 11:36:11 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<num.length();i++){
+            for(int j=i+1;;j<num.length();j++){
+                if(num[i]+num[j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
