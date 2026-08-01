a0fd32cb7b538a83cc2b64f1a9fb2536e7baabe6
Jasica Aggarwal <jasicaaggarwal50@gmail.com>
2026-08-01 11:00:28 +0530

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+     HashMap<Integer,Integer> map= new HashMap<>();
+        for(int i:nums){
+            if(map.contains(i)){
+                return true;
+            }
+            map.put(i,1);
+            else{
+                return false;
+            }
+        }
+    }
+}
